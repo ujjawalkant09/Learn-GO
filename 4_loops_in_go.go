6c0b6023cf9a3a7a1a4f7298bc6151240d2dfcb0
ujjawalkant <48159084+ujjawalkant09@users.noreply.github.com>
2025-02-27 22:37:29 +0530

    Loops in go

---
+++ 4_loops_in_go.go
@@ -0,0 +1,26 @@
+// Loops
+// For Loop
+// Notes :-> Go has only for loops (no while or do-while loops).
+// FYI, the following code sections are commented out. 
+
+
+
+package main
+
+import "fmt"
+
+// func main(){
+// 	for i:=0;i<=5;i++{
+// 		fmt.Println(i)
+// 	}
+// }
+
+
+// Range (for-each loop)
+// func main(){
+// 	number := []int{10,20,30}
+
+// 	for index,value := range number{
+// 		fmt.Println("Index : ",index,"Value:",value)
+// 	}
+// }
