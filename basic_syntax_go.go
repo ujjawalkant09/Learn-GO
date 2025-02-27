82cf1f554daee63133515d18d84c25ecbec05bb0
ujjawalkant <48159084+ujjawalkant09@users.noreply.github.com>
2025-02-27 20:50:13 +0530

    Basic Syntax in Go

---
+++ basic_syntax_go.go
@@ -0,0 +1,15 @@
+"
+1. Basic Syntax in Go
+Hello World
+
+"
+
+package main  // Main package
+
+import "fmt"  // Import fmt for printing
+
+func main() {
+    fmt.Println("Hello, World!")  // Print output
+}
+
+
